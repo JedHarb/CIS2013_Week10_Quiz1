e64c2a5d4a5cb0dedd74f82840751875cf38e94c
JedHarb <jedharb@gmail.com>
2018-03-28 18:14:07 -0500

    finished

--- CIS2013_Week10_Quiz1/Source.cpp
+++ CIS2013_Week10_Quiz1/Source.cpp
@@ -6,13 +6,27 @@ using namespace std;
 int main() {
 
 	string userInput;
+	char i;
+	int count = 0;
+	char keepAlive = 'y';
 
 	cout << "Please type some input that includes the letter 'b': " << endl;
-	cin.ignore();
 	getline(cin, userInput);
+	for (i = 0; i < userInput.length(); i++) {
+		if (userInput[i] == 'b' || userInput[i] == 'B') {
+			count++;
+			if (count % 2 == 0) {
+				cout << "STAND";
+			}
+			else {
+				cout << "SIT";
+			}
+		}
+		else cout << userInput[i];
+	}
 
-
-
+	cout << endl << " End of program ";
+	cin >> keepAlive; //keep the program from closing the window until the user inputs something
 
 return 0;
 }
