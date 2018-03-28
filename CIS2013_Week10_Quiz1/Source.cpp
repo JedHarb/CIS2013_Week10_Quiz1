707eb3d9457a8498f4e9afdf77d5e9e095218e60
JedHarb <jedharb@gmail.com>
2018-03-28 18:00:56 -0500

    Week 10 quiz, replacing a character

---
+++ CIS2013_Week10_Quiz1/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+#include<string> 
+using namespace std;
+
+int main() {
+
+	string userInput;
+
+	cout << "Please type some input that includes the letter 'b': " << endl;
+	cin.ignore();
+	getline(cin, userInput);
+
+
+
+
+return 0;
+}
